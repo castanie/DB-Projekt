5024e2ea6791fb451981b01cc10fdab3f310ce6a
CalaVera2xPlus <mislav.buffon1@gmail.com>
2021-07-02 19:55:38 +0200

    Updated TestInstance.

--- app/src/test/java/Projekt/AppTest.java
+++ app/src/test/java/Projekt/AppTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.*;
 
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AppTest {
 
     Connection conn;
